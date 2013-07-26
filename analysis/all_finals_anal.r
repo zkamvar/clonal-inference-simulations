feab7c781411480d0dba14ef68957d1c693e2df8
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2013-07-26 09:51:40 -0700

    modified plots
    Former-commit-id: bec0b33bdb546b4f79fe065e3bf8433d0de05b29

--- analysis/all_finals_anal.r
+++ analysis/all_finals_anal.r
@@ -5,8 +5,8 @@ sim$Samp.Size <- factor(sim$Samp.Size, levels = c(10,25,50,100))
 
 simnarm <- sim[-which(is.na(sim$rbarD)), ]
 
-Ia.bar <- ggplot(simnarm) + geom_boxplot(aes(x = factor(Generation*1000), y = Ia, fill = Samp.Size)) + facet_wrap(~Sex.Rate) + theme_bw() + scale_fill_grey(start = 0.4, end = 1)
-rbarD.bar <- ggplot(simnarm) + geom_boxplot(aes(x = factor(Generation*1000), y = rbarD, fill = Samp.Size)) + facet_wrap(~Sex.Rate) + theme_bw() + scale_fill_grey(start = 0.4, end = 1)
+Ia.bar <- ggplot(simnarm) + geom_boxplot(aes(x = factor(Generation*1000), y = Ia, fill = Samp.Size), outlier.shape = 20, outlier.size = 1, notch = TRUE) + facet_wrap(~Sex.Rate, scales = "free_y", nrow = 2) + theme_bw() + scale_fill_grey(start = 0.4, end = 1)
+rbarD.bar <- ggplot(simnarm) + geom_boxplot(aes(x = factor(Generation*1000), y = rbarD, fill = Samp.Size), outlier.shape = 20, outlier.size = 1, notch = TRUE) + facet_wrap(~Sex.Rate, scales = "free_y", nrow = 2) + theme_bw() + scale_fill_grey(start = 0.4, end = 1)
 
 
 Ia.point <- ggplot(simnarm, aes(x = factor(Generation*1000), y = Ia, color = Samp.Size, group = Samp.Size, alpha = 0.5)) + geom_point(position = "jitter") + stat_smooth() + facet_grid(Sex.Rate ~ Samp.Size) + theme_bw()
