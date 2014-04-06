bdd79a56a20932a867f7ce112ddef9a973fe3349
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2014-04-06 15:13:06 -0700

    added file clonal_schemes.py Eventually will be master file
    Former-commit-id: 581ed57e25b57ea8a6ba23f4fcad7080dc28c1e6

---
+++ clonal_schemes.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import simuPOP as sim
+import os, sys, datetime, commands, re, exceptions, multiprocessing, random, numpy
+from simuOpt import setOptions
+from simuPOP import utils
+from simuPOP.utils import export
+from simuPOP.sampling import drawRandomSample, drawRandomSamples 
+
+
+#------------------------------------------------------------------------------#
+# clone simulator in case the population goes strictly clonal.
+# newG0 is the number of generations left for the population to go.
+# This will also create a population marking the point at which all individuals
+# are of the same sex (or mating type). 
+#------------------------------------------------------------------------------#
+def goToClone(pops, L0, newG0, G0, N0, R0):   
+    simu = sim.Simulator(pops)
+	print("GOING TO CLONAL REPRODUCTION")
+	popsize = r"'Pop Size: %d'"
+	males = r"'Number of Males: %d'"
+	het = r"'Heterozygosity: %.2f'"
+	generations = r"'Gen: %d'"
+
+	stats = " | ".join([popsize, males, het, generations])
+    simu.evolve(  
+        matingScheme = sim.RandomSelection(subPops=0),
+        preOps=[sim.StepwiseMutator(rates=1e-5, loci=L0)],
+        postOps=[
+            # This outputs more informative statistics to the screen. 
+            sim.Stat(popSize=True, numOfMales=True, heteroFreq=L0, step=G0/10),
+            sim.PyEval(r"'Pop Size: %d | Number of Males: %d | Heterozygosity: \
+%.2f | Gen: %d\n' % (popSize, numOfMales, heteroFreq[1], gen-("+str(G0)+"/10))", step=G0/10),
+            sim.SavePopulation(output="!'"+str(N0)+"/"+str(N0)+
+            "_gen_%d.pop' % (gen-("+str(G0)+"/10))", step=G0/10),
+        ],
+        finalOps=[
+            sim.SavePopulation(output="!'"+str(N0)+"/"+str(N0)+
+            "_gen_%d.pop' % (gen-("+str(G0)+"/10))", step=G0/10),
+        ],
+        gen = newG0
+    )
+    print("\n---\nRep "+str(R0)+" is done!\n---\n")
