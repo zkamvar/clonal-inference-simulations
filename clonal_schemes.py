bf82cc92f1981e4f126844aee531f6dab16c8432
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2014-04-06 18:07:38 -0700

    added more options to script
    Former-commit-id: 564ec16eaff7e36c8061c76db75a0adf81bed823

--- clonal_schemes.py
+++ clonal_schemes.py
@@ -45,6 +45,15 @@ def goToClone(pops, L0, newG0, G0, N0, R0):
 
 print("Hey there!")
 
+#------------------------------------------------------------------------------#
+# Variables to set up 
+#------------------------------------------------------------------------------#
+C0 = 90
+STEPS = 10
+GENERATIONS = 1000
+
+# Initializing a population of 100 individuals with two loci each on separate
+# chromosomes. These loci each have 4 alleles.
 pop = sim.Population(
     size = 100, 
     loci = [1, 1], 
@@ -52,33 +61,67 @@ pop = sim.Population(
     alleleNames = '201 202 204 206'.split()
     )
 
+#------------------------------------------------------------------------------#
+# Generating initializing operators.
+#------------------------------------------------------------------------------#
+
+# This will generate and plot allele probabilities for each locus.
 loclist = get_allele_probabilities(2, 4)
 plot_allele_probabilities(loclist, 4)
 
+# This will put all of the initializing steps into a list. 
+# 1. initialize sex for the populations
+# 2. initialize genotypes for each locus separately.
 inits = list()
 inits.append(sim.InitSex())
 for i in range(len(loclist)):
     inits.append(sim.InitGenotype(freq = loclist[i], loci = i))
 
+#------------------------------------------------------------------------------#
+# Generating stats operators.
+#------------------------------------------------------------------------------#
+# The stats operators must be raw strings. Since it would be advantageous later
+# to be able to add in different stats, it's better to join a bunch of small
+# strings than write one long one.
+
+# The stats must have single quotes around them.
+foot = r"\n'"
+head = r"'"
+
 popsize = r"Pop Size: %d"
 males = r"Number of Males: %d"
 het = r"Heterozygosity: %.2f %.2f"
 generations = r"Gen: %d"
-foot = r"\n'"
-head = r"'"
+
+# Joining the statistics together with pipes.
 stats = " | ".join([head,popsize, males, het, generations, foot])
+
+# The string for the evaluation of the stats. 
 stateval = " % (popSize, numOfMales, heteroFreq[0], heteroFreq[1], gen)"
-stats = stats + stateval
 
-statargs = sim.Stat(popSize=True, numOfMales=True, heteroFreq=[0, 1], step = 1)
-evalargs = sim.PyEval(stats, step = 1)
+# Stat and PyEval are both classes, so they can be put into variables. These
+# will be evaluated as the program runs. 
+statargs = sim.Stat(popSize=True, numOfMales=True, heteroFreq=[0, 1], step = STEPS)
+evalargs = sim.PyEval(stats + stateval, step = STEPS)
 
+
+#------------------------------------------------------------------------------#
+# Generating mating.
+#------------------------------------------------------------------------------#
+
+rand_mate = sim.RandomMating(subPops = 0, weight = C0)
+clone_mate = sim.RandomSelection(subPops = 0, weight = 100 - C0)
+mate_scheme = sim.HeteroMating([rand_mate, clone_mate])
+
+#------------------------------------------------------------------------------#
+# Do the evolution.
+#------------------------------------------------------------------------------#
 pop.evolve(
     initOps = inits,
-    matingScheme = sim.RandomMating(),
+    matingScheme = mate_scheme,
     preOps = [sim.StepwiseMutator(rates = 1e-5, loci = [1,1])],
     postOps = [statargs, evalargs],
-    gen = 10
+    gen = GENERATIONS
     )
 
 
