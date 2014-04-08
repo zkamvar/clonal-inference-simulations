f1f89b334d360d8f01e40b0bf1f7f845a4b15304
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2014-04-08 14:09:02 -0700

    Added function to generate allele names
    Former-commit-id: 06392681b1256a037e7e190edca84c3b794c9efe

--- random_alleles.py
+++ random_alleles.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+import sys
 
 
 def rescale_allele_probabilities(allele_probs, nloc, nall):
@@ -43,3 +44,30 @@ def plot_allele_probabilities(allele_probs, nall):
 			count = count + 1
 			print(str(count)+": "+"="*int(round(i[j]*100))+" "+str(round(i[j], 3)))
 	return
+
+'''
+Generate names of microsatellite alleles.
+
+Inputs:
+	nloc: an integer defining the number of loci.
+	nall: an integer defining the number of alleles per locus.
+
+Output:
+	A list of lists of allele names. Each locus will have a separate repeat
+	length defined randomly. This repreat length will be reflected in the allele
+	names. So, if the repeat is of length 3 (CAT)^n, then the allele names will
+	be '3XX' and they will be divisible by 3.
+'''
+def get_allele_names(nloc, nall):
+	replens = np.random.random_integers(2, 6, nloc)
+	locus_list = list()
+	for i in range(len(replens)):
+		prefix = replens[i]
+		allele_list = list()
+		for j in range(nall):
+			allele = str((prefix * 100) + prefix * (j + 1))
+			sys.stdout.write(allele + " ")
+			allele_list.append(allele)
+		locus_list.append(allele_list)
+		print("\n")
+	return locus_list
