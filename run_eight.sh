577d60354f338f1b4cdf544176ec99859ff5242c
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-08-19 15:48:28 -0700

    see if piping works

--- run_eight.sh
+++ run_eight.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
-python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt1 &
-python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt2 &
-python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt3 &
-python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt4 &
-python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt5 &
-python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt6 &
-python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt7 &
-python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt8
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt1 > pt1.log 2>&1 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt2 > pt1.log 2>&1 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt3 > pt1.log 2>&1 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt4 > pt1.log 2>&1 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt5 > pt1.log 2>&1 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt6 > pt1.log 2>&1 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt7 > pt1.log 2>&1 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --outfile pt8 > pt1.log 2>&1
