787035519a3def088366ece04e0a65cdc6ed5945
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-08-19 15:24:15 -0700

    add file to run a bunch of simulations at once
    This will hopefully run 12 simulations over all modes of sexual
    reproduction over the 8 cores of Javi's machine.

---
+++ run_eight.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+python simulations/simulate_sex_rate.py --nseed 12 --output pt1 &
+python simulations/simulate_sex_rate.py --nseed 12 --output pt2 &
+python simulations/simulate_sex_rate.py --nseed 12 --output pt3 &
+python simulations/simulate_sex_rate.py --nseed 12 --output pt4 &
+python simulations/simulate_sex_rate.py --nseed 12 --output pt5 &
+python simulations/simulate_sex_rate.py --nseed 12 --output pt6 &
+python simulations/simulate_sex_rate.py --nseed 12 --output pt7 &
+python simulations/simulate_sex_rate.py --nseed 12 --output pt8
