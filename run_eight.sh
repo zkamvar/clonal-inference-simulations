6bba0825105aeeb0a6433ffdd903a7a58fc2393f
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-08-19 15:35:55 -0700

    update popsize

--- run_eight.sh
+++ run_eight.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
-python simulations/simulate_sex_rate.py --nseed 12 --output pt1 &
-python simulations/simulate_sex_rate.py --nseed 12 --output pt2 &
-python simulations/simulate_sex_rate.py --nseed 12 --output pt3 &
-python simulations/simulate_sex_rate.py --nseed 12 --output pt4 &
-python simulations/simulate_sex_rate.py --nseed 12 --output pt5 &
-python simulations/simulate_sex_rate.py --nseed 12 --output pt6 &
-python simulations/simulate_sex_rate.py --nseed 12 --output pt7 &
-python simulations/simulate_sex_rate.py --nseed 12 --output pt8
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --output pt1 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --output pt2 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --output pt3 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --output pt4 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --output pt5 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --output pt6 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --output pt7 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 12 --output pt8
