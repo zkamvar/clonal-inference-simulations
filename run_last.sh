c4e25cacb9269e670de958eaa9b12519ad19c311
Javier Felipe Tabima Restrepo <javier.tabima@arsorcor1ngtec2.usda-ars.orst.edu>
2016-08-25 10:15:23 -0700

    add script to run final 4 sims

---
+++ run_last.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 1 --outfile pt9 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 1 --outfile pt10 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 1 --outfile pt11 &
+python simulations/simulate_sex_rate.py --POPSIZE 10000 --nseed 1 --outfile pt12 
+
