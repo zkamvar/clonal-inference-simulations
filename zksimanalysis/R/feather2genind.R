5dbd28c891c2ff6b755d725d33b2b9478975951b
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-09-06 17:25:33 -0700

    Add example_data function

--- zksimanalysis/R/feather2genind.R
+++ zksimanalysis/R/feather2genind.R
@@ -10,7 +10,21 @@
 #' @export
 #'
 #' @examples
-#'
+#' library("tidyr")
+#' ex  <- example_data()
+#' gid <- feather2genind(ex)
+#' # split the population name into its components
+#' ex_run  <- "twenty_loci([0-9]+?)/"
+#' ex_seed <- "seed_([0-9]+?)_"
+#' ex_sex  <- "sex_([0-9.]+?)_"
+#' ex_gen  <- "gen_([0-9]+?)_"
+#' ex_rep  <- "rep_([0-9]+?).pop"
+#' strata(gid) <- strata(gid) %>%
+#'   extract(pop, c("run", "seed", "sexrate", "gen", "rep"),
+#'           paste0(ex_run, ex_seed, ex_sex, ex_gen, ex_rep),
+#'           remove = FALSE)
+#' setPop(gid) <- ~run/rep/seed
+#' gid
 feather2genind <- function(ff, locus_prefix = "Locus", sample = 50, genclone = TRUE, verbose = FALSE){
   if (verbose) message(paste0("Reading ", ff, " ..."))
   fdf  <- feather::read_feather(ff) %>% group_by_(~pop) %>% sample_n(sample)
@@ -55,4 +69,13 @@ tidy_ia <- function(gid, ...){
   return(data_frame_(out))
 }
 
-
+#' A wrapper to get example data
+#'
+#' @return a string with the path to the example
+#' @export
+#'
+#' @examples
+#' example_data()
+example_data <- function(){
+  system.file("files", "sample_data.feather", package = "zksimanalysis")
+}
