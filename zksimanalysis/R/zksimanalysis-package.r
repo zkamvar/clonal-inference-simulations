0b263367cdc0b7eeba797d50ad0053c83088022d
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-09-06 17:32:08 -0700

    Add roxygen imports to namespace

---
+++ zksimanalysis/R/zksimanalysis-package.r
@@ -0,0 +1,20 @@
+#' zksimanalysis.
+#'
+#' @name zksimanalysis
+#' @docType package
+#'
+#' @description
+#' This package is for analysis of simulations created via simuPOP.
+#'
+#' @section functions:
+#'
+#' \itemize{
+#'  \item \code{\link{feather2genind}}
+#'  \item \code{\link{tidy_ia}}
+#'  \item \code{\link{example_data}}
+#' }
+#'
+#' @import poppr
+#' @import dplyr
+#' @import feather
+NULL
