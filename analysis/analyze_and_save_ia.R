c373b61c32506e91ed621cbf0bc516c9f3c0eb32
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-09-08 15:59:27 -0700

    add arguments

--- analysis/analyze_and_save_ia.R
+++ analysis/analyze_and_save_ia.R
@@ -1,1 +1,13 @@
-#!/usr/bin/env R
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages(library("optparse"))
+option_list <- list(
+    make_option(c("-f", "--files"), action = "store", type = "character",
+                help = "Feather formatted file(s) to analyze"),
+    make_option(c("-s", "--sample"), action = "store", type = "integer",
+                default = c(10L, 25L, 50L, 100L),
+                help = "Number of individuals to randomly sample from each population"),
+    make_option(c("-p", "--permutations"), action = "store", type = "integer",
+                default = 99L,
+                help = "Number of permuatations to perform to calculate significance."),
+
+)
