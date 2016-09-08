015e23081766e568b3faf84fa12436dd1c0ccc23
Zhian Kamvar <kamvarz@dayhoff.cgrb.oregonstate.local>
2016-09-08 11:50:38 -0700

    update bash file to work properly

--- analysis/unzip_and_analyze.sh
+++ analysis/unzip_and_analyze.sh
@@ -6,7 +6,6 @@
 theFile=$1
 theScript=$2
 
-
 # If there are, create a new directory and assign that to TMPDIR
 if [ "$(ls -A $TMPDIR)" ]; then
 	mkdir $TMPDIR/znk
@@ -16,5 +15,9 @@ fi
 # Unzip the file into TMPDIR
 unzip -j -d $TMPDIR $theFile
 
-# Run the script
-$theScript $(ls $TMPDIR)
+# Create the command by concatenating the results of the unzipping
+# and the script to run. Note, for multiple arguments within a
+# script, quoting is encouraged (e.g. "ls -l")
+cmd=$theScript" "$(ls -d $TMPDIR/*)
+echo $cmd
+eval $cmd
