581507695d126ebb95d1880372c4c7679385efa0
Javier Felipe Tabima Restrepo <javier.tabima@arsorcor1ngtec2.usda-ars.orst.edu>
2016-09-12 13:14:31 -0700

    update analysis script

--- analysis/post_analysis.R
+++ analysis/post_analysis.R
@@ -5,24 +5,24 @@ library('tidyr')
 library('purrr')
 library('ggplot2')
 
-datfiles <- dir("simulation_analysis/rda_files/", full.names = TRUE) %>% grep("feather.rda$", ., value = TRUE)
+datfiles <- dir("rda_files/", full.names = TRUE) %>% grep("feather.rda$", ., value = TRUE)
 twenties <- grep("twenty", datfiles, value = TRUE)
 full     <- grep("twenty", datfiles, value = TRUE, invert = TRUE)
 
 
-for (i in twenties){
+for (i in full){
   cat(i, "\n")
   load(i)
 }
 
-ex_run  <- "twenty_loci([0-9]+?)/"
+ex_run  <- "pt([0-9]+?)/"
 ex_seed <- "seed_([0-9]+?)_"
 ex_sex  <- "sex_([0-9.]+?)_"
 ex_gen  <- "gen_([0-9]+?)_"
 ex_rep  <- "rep_([0-9]+?).pop"
 ex_samp <- "_sam_([0-9]+?)$"
 
-datnames <- grep("^X.+?feather$", ls(), value = TRUE)
+datnames <- grep("^X.+?_pt.+?feather$", ls(), value = TRUE)
 
 datalist <- datnames %>%
   lapply(get) %>%
@@ -47,11 +47,15 @@ ggplot(vals, aes(x = sexrate, y = rbarD, fill = sample)) +
 
 ggplot(vals, aes(x = sexrate, y = p.rD, fill = sample)) +
   geom_boxplot() +
-  scale_y_log10()
+  scale_y_log10(breaks = c(0.01, 0.1, 1),
+                minor_breaks = c((1:9)*0.001, (2:9)*0.01, (2:9)*0.1))
 
 ggplot(vals, aes(x = sexrate, y = rbarD, color = log(p.rD))) +
-  geom_point(position = "jitter", alpha = 0.5) +
+  geom_point(alpha = 0.25, position = "jitter") +
   geom_boxplot(color = "black", notch = TRUE, width = 0.5, alpha = 0.25) +
-  scale_color_viridis(option = "magma", breaks = log(c(0.005, 0.01, 0.025, 0.05, 0.1)), labels = c(0.005, 0.01, 0.025, 0.05, 0.1)) +
-  facet_wrap(~sample)
+  scale_color_viridis(option = "viridis", breaks = log(c(0.005, 0.01, 0.025, 0.05, 0.1)), labels = c(0.005, 0.01, 0.025, 0.05, 0.1)) +
+  facet_wrap(~sample, nrow = 1)
+
+
+
 
