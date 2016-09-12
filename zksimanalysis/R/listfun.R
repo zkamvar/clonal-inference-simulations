e879288f3d77f840aa37947f1e9cb00416bae94c
Javier Felipe Tabima Restrepo <javier.tabima@arsorcor1ngtec2.usda-ars.orst.edu>
2016-09-12 10:27:50 -0700

    update listfun to take in extra args

--- zksimanalysis/R/listfun.R
+++ zksimanalysis/R/listfun.R
@@ -4,14 +4,15 @@
 #'
 #' @param x a single element list
 #' @param f a function to apply to the first element of x
+#' @param ... any arguments that go with f
 #'
 #' @return the output of f
 #' @export
 #'
 #' @examples
-#' x <- list(y = 1:10)
-#' listfun(x, sum)
-listfun <- function(x, f){
+#' x <- list(y = c(1:10, NA))
+#' listfun(x, sum, na.rm = TRUE)
+listfun <- function(x, f, ...){
   f <- match.fun(f)
-  f(x[[1]])
+  f(x[[1]], ...)
 }
