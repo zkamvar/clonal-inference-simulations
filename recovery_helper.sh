9ad3c3ae2081de11793e49d706b73130902b89dc
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-10-12 16:45:52 -0700

    four spt

--- recovery_helper.sh
+++ recovery_helper.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-  echo
-  echo "Rerun unused simulations"
-  echo
-  echo "Usage:"
-  echo
-  echo "    bash recovery_helper.sh <failed simulation text file>"
-  echo
-  exit
+	echo
+	echo "Rerun unused simulations"
+	echo
+	echo "Usage:"
+	echo
+	echo "		bash recovery_helper.sh <failed simulation text file>"
+	echo
+	exit
 fi
 
 # Reading in file line by line
@@ -17,9 +17,9 @@ foo=( )
 
 counter=0
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    # echo "Text read from file: $line; counter $counter"
-    foo[$counter]=$line
-    let counter=counter+1
+	# echo "Text read from file: $line; counter $counter"
+	foo[$counter]=$line
+	let counter=counter+1
 done < "$1" 
 
 PIDS=( )
@@ -27,18 +27,18 @@ counter=0
 # Looping over an array with index
 # http://stackoverflow.com/a/6723516
 for i in "${!foo[@]}"; do 
-  if ! ((i % 8)); then
-    # For the PID waiting
-    # http://stackoverflow.com/a/356154
-    let counter=0
-    for pid in "${!PIDS[@]}"; do
-      printf "waiting on ${PID[$pid]}\n"
-      wait ${PID[$pid]}
-    done
-  fi
-  printf "%s\t%s\n" "$i" "${foo[$i]}"
-  eval ${foo[$i]} & 
-  PIDS[$counter]=$!
-  let counter=counter+1
+	if ! ((i % 8)); then
+		# For the PID waiting
+		# http://stackoverflow.com/a/356154
+		let counter=0
+		for pid in "${!PIDS[@]}"; do
+			printf "waiting on ${PID[$pid]}\n"
+			wait ${PID[$pid]}
+		done
+	fi
+	printf "%s\t%s\n" "$i" "${foo[$i]}"
+	eval ${foo[$i]} & 
+	PIDS[$counter]=$!
+	let counter=counter+1
 done
 wait
