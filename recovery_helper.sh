514bc9f540254bf861796c659ab61cfd1b00802b
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-10-13 10:38:46 -0700

    add concurrency to recovery_helper

--- recovery_helper.sh
+++ recovery_helper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
+if [ $# -lt 2 ]; then
 	echo
 	echo "Rerun unused simulations"
 	echo
 	echo "Usage:"
 	echo
-	echo "		bash recovery_helper.sh <failed simulation text file>"
+	echo "		bash recovery_helper.sh <number of concurrent processes> <failed simulation text file>"
 	echo
 	exit
 fi
@@ -20,14 +20,14 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	# echo "Text read from file: $line; counter $counter"
 	foo[$counter]=$line
 	let counter=counter+1
-done < "$1" 
+done < "$2" 
 
 PIDS=( )
 counter=0
 # Looping over an array with index
 # http://stackoverflow.com/a/6723516
 for i in "${!foo[@]}"; do 
-	if ! ((i % 8)); then
+	if ! ((i % $1)); then
 		# For the PID waiting
 		# http://stackoverflow.com/a/356154
 		let counter=0
