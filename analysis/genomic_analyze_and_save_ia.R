94501305ac55ad2fea15d4712d527e4799750f25
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-10-14 13:48:16 -0700

    working version of genomic_analyze_and_save

--- analysis/genomic_analyze_and_save_ia.R
+++ analysis/genomic_analyze_and_save_ia.R
@@ -5,19 +5,19 @@ suppressPackageStartupMessages(library("stringr"))
 ================================================================================
 Parse feather formatted data, analyze the index of association and save the result
 
-Usage: analyze_and_save_ia.R [-dvkc -s SEED [-n NSAMPLE...] -p PERMUTATIONS -l LOCUS -o PATH] [FILE...]
+Usage: analyze_and_save_ia.R [-dvk [-m MISSING...] -s SEED [-n NSAMPLE...] -p PERMUTATIONS -l LOCUS -o PATH] [FILE...]
 
 Options:
  -h,--help                                    show this message and exit
  -v,--verbose                                 record progress
  -d,--debug                                   record EVERYTHING
  -k,--keep                                    keep the data in the resulting data frame
- -c,--clonecorrect                            add values from clone-correction
+ -m MISSING...,--missing=MISSING              include analyze missing data percentage (no permutation) [default 0.01 0.05 0.10]
  -s SEED,--seed=SEED                          random seed [default: 20160909]
  -n NSAMPLE...,--nsample=NSAMPLE...           number of samples/population (single quoted list of integers) [default: 10 25 50 100]
  -p PERMUTATIONS,--permutations=PERMUTATIONS  number of permutations for the index of association [default: 99]
- -l LOCUS,--locus=LOCUS                       prefix to identify the locus columns [default: Locus]
- -o PATH,--output=PATH                        default path to place Rdata files [default: ~/rda_files]
+ -l LOCUS,--locus=LOCUS                       prefix to identify the locus columns [default: ^[0-9]+?_[0-9]+?$]
+ -o PATH,--output=PATH                        default path to place Rdata files [default: ~/genomic_rda_files]
 
 ================================================================================
 " -> doc
@@ -35,6 +35,7 @@ if (length(opt$FILE) == 0){
 opt$seed         <- as.integer(opt$seed)
 opt$permutations <- as.integer(opt$permutations)
 opt$nsample      <- as.integer(str_split(opt$nsample, "[[:blank:]]")[[1]])
+opt$missing      <- as.numeric(str_split(opt$missing, "[[:blank:]]")[[1]])
 
 totsamp <- sum(opt$nsample)
 if (!dir.exists(opt$output)){
@@ -55,59 +56,83 @@ suppressPackageStartupMessages(library("zksimanalysis"))
 suppressPackageStartupMessages(library("purrr"))
 
 
+
+# Helper Functions --------------------------------------------------------
+
+# Full analysis here takes the data, separates, populations, and analyzes
+# $\bar{r}_d$ with permutations
+full_analysis <- . %>%
+  seppop() %>%
+  map(tidy_ia,
+      sample   = opt$permutations,
+      keepdata = opt$keep,
+      verbose  = opt$verbose,
+      quiet    = !opt$debug) %>%
+  bind_rows()
+
+# Missing analysis will calculate the value of $\bar{r}_d$ for all the data 
+# sets with missing data and bind them into a tibble.
+missing_analysis_match <- . %>%
+  map(seppop) %>%
+  map(map_dbl, 
+      bitwise.ia,
+      threads = 1L) %>%
+  bind_rows()
+
+missing_analysis_nomatch <- . %>%
+  map(seppop) %>%
+  map(map_dbl, 
+      bitwise.ia,
+      missing_match = FALSE,
+      threads = 1L) %>%
+  bind_rows()
+
 # Processing data ---------------------------------------------------------
-#
-# Here, each file is processed by:
-#  1. sampling a sum of all the samples from the file
-#     This is done as a sum of all the sample sizes to reduce read time and
-#     ensure sampling without replacement.
-#  2. adding a strata for each sample size
-#  3. splitting the populations by the sample sizes
-#  4. calculating the index of association with permutation for each population
-#  5. binding these results into a tidy data frame
-#  6. saving this as a file in the output directory
+
 for (f in opt$FILE){
   set.seed(opt$seed) # same seed for each file
-  indat <- feather2genind(f, locus_prefix = opt$locus, sample = totsamp, verbose = opt$verbose) %>%
+  indat <- feather2genlight(f, locus_regex = opt$locus, sample = totsamp, verbose = opt$verbose) %>%
     setPop(~pop)
   inpops  <- nPop(indat)
   allpops <- inpops * length(opt$nsample)
   subpops <- rep(opt$nsample, inpops) %>%
     rep(., .) %>%
     paste0("sam_", .)
-  if (opt$verbose) message(paste("Analyzing", allpops, "populations ... "))
-  res <- indat %>%
+
+  # Processing the incoming data so that it can be split into subpopulations.
+  indat <- indat %>%
     addStrata(data.frame(sample_size = subpops)) %>%
-    setPop(~pop/sample_size) %>%
-    seppop() %>%
-    map(tidy_ia,
-        sample   = opt$permutations,
-        keepdata = opt$keep,
-        cc       = opt$clonecorrect,
-        strata   = ~pop/sample_size,
-        verbose  = opt$verbose,
-        hist     = FALSE,
-        quiet    = !opt$debug) %>%
-    bind_rows()
-  if (opt$mutant){
-  # At this point, if the first locus is a locus with a higher mutation rate
-  # than the other loci, It is removed, MLGS recalculated, and everything is
-  # processed again.
-    if (opt$verbose) message(paste("Analyzing populations without first locus ... "))
-    mres <- indat[loc = -1, mlg.reset = TRUE] %>%
-      addStrata(data.frame(sample_size = subpops)) %>%
-      setPop(~pop/sample_size) %>%
-      seppop() %>%
-      map(tidy_ia,
-          sample   = opt$permutations,
-          keepdata = FALSE,
-          cc       = opt$clonecorrect,
-          strata   = ~pop/sample_size,
-          verbose  = opt$verbose,
-          hist     = FALSE,
-          quiet    = !opt$debug) %>%
-      bind_rows()
+    setPop(~pop/sample_size)
+
+  # Set missing will create a list of data sets with missing data. The input is
+  # the vector of missing percentages.
+  set_missing <- . %>% {
+    set.seed(opt$seed)
+    pop_NA(indat, na.perc = ., parallel = FALSE)
   }
+  miss <- map(opt$missing, set_missing) %>% 
+    setNames(paste0("missing_", opt$missing)) 
+  
+  if (opt$verbose) message(paste("Analyzing", allpops, "populations ... "))
+  # Analyze $\bar{r}_d$ with permutations
+  res  <- indat %>% 
+    full_analysis
+  if (opt$verbose) message(paste("Analyzing missing data (with matching) ... "))
+  # Analyze $\bar{r}_d$ with missing data (missing data always matches)
+  mres <- miss %>%
+    missing_analysis_match
+  if (opt$verbose) message(paste("Analyzing missing data (no matching) ... "))
+  # Analyze $\bar{r}_d$ with missing data (missing data never matches)
+  mres_nomatch <- miss %>%
+    setNames(paste0("missing_nomatch_", opt$missing)) %>%
+    missing_analysis_nomatch
+  # Preserve the augmented data sets.
+  mdat <- miss %>% 
+    setNames(paste0("dataset_", opt$missing)) %>%
+    map(seppop) %>% 
+    bind_rows()
+  res <- bind_cols(mres, mres_nomatch, res, mdat)
+  
   # Saving everything.
   outf <- make.names(f)
   if (opt$keep){
@@ -123,14 +148,6 @@ for (f in opt$FILE){
   outf_location <- paste0(opt$output, "/", outf, ".rda")
   if (opt$verbose) message(paste("saving results to", outf_location))
   save(list = outf, file = outf_location)
-
-  if (opt$mutant){
-    moutf <- paste0(outf, ".mutant")
-    assign(x = moutf, mres)
-    moutf_location <- paste0(opt$output, "/", moutf, ".rda")
-    if (opt$verbose) message(paste("saving mutant results to", moutf_location))
-    save(list = moutf, file = moutf_location)
-  }
 }
 if (opt$verbose){
   options(width = 100)
