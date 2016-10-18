51a96e9c7327e1c115ab4e52dcf3a76e81808c8c
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-10-18 13:29:10 -0700

    shuffle_genlight can now shuffle by block

--- zksimanalysis/R/shuffle_genlight.R
+++ zksimanalysis/R/shuffle_genlight.R
@@ -2,6 +2,7 @@
 #'
 #' @param x a snpclone or genlight object
 #' @param mat a matrix derived from x
+#' @param blocksize the size of the blocks at which genotypes should be shuffled.
 #'
 #' @return a snpclone or genlight object
 #' @export
@@ -13,10 +14,42 @@
 #' gls <- shuffle_genlight(gl, as.matrix(gl))
 #' gls
 #' bitwise.ia(gls)
-shuffle_genlight <- function(mat, x){
-  shuff <- new("genlight",
-               apply(mat, 2, sample),
-               parallel = FALSE)
+shuffle_genlight <- function(mat, x, blocksize = 100){
+
+  if (blocksize == 1){
+    shuff <- new("genlight", apply(mat, 2, sample), parallel = FALSE)
+  } else {
+    if (is.null(position(x))){
+      position(x) <- seq(nLoc(x))
+    }
+    pos <- position(x)
+    if (!is.null(chromosome(x))){
+      # Each chromosome has it's own position.
+      # In this case, get large round number for each chromosome break.
+      maxp <- 10^ceiling(log(max(pos), 10))
+      if (length(unique(pmin(pos))) < nLoc(x)){
+        pos <- split(pos, chromosome(x))
+        for (p in seq(pos)){
+          if (p != 1){
+            pos[[p]] <- pos[[p]] + (maxp*(p - 1))
+          }
+        }
+        pos <- unlist(pos, use.names = FALSE)
+      }
+    }
+    blocks   <- seq(blocksize, max(pos), by = blocksize)
+    blockmat <- matrix(blocks, nrow = length(blocks), ncol = 2)
+    blockmat[, 1] <- blockmat[, 1] - blocksize + 1
+
+    for (i in seq(blocks)){
+      blockrange <- pos %in% blockmat[i, 1]:blockmat[i, 2]
+      if (any(blockrange)){
+        s <- sample(nrow(mat))
+        mat[, blockrange] <- mat[s, blockrange]
+      }
+    }
+  }
+  shuff <- new("genlight", mat, parallel = FALSE)
   x@gen <- shuff@gen
   return(x)
 }
@@ -25,6 +58,7 @@ shuffle_genlight <- function(mat, x){
 #'
 #' @param x a genlight or snpclone object
 #' @param mat a matrix derived from x
+#' @param blocksize the size of the blocks at which genotypes should be shuffled.
 #'
 #' @return a value of the index of association
 #' @export
@@ -33,14 +67,15 @@ shuffle_genlight <- function(mat, x){
 #' gl <- glSim(100, 100, 100, parallel = FALSE)
 #' bitwise.ia(gl)
 #' sample_bitwise_ia(gl, as.matrix(gl))
-sample_bitwise_ia <- function(mat, x){
-  bitwise.ia(shuffle_genlight(mat, x), threads = 1L)
+sample_bitwise_ia <- function(mat, x, blocksize = 100){
+  bitwise.ia(shuffle_genlight(mat, x, blocksize), threads = 1L)
 }
 
 #' A test for significance with genomic ia
 #'
 #' @param x a genlight or snpclone object
 #' @param sample the number of samples for significance testing
+#' @param blocksize the size of the blocks at which genotypes should be shuffled.
 #'
 #' @return a list with the observed value and pvalue and the samples used.
 #' @export
@@ -52,14 +87,14 @@ sample_bitwise_ia <- function(mat, x){
 #' @examples
 #' gl <- glSim(100, 200, 0, parallel = FALSE)
 #' genomic_ia(gl, sample = 999)
-genomic_ia <- function(x, sample = 0, quiet = FALSE){
+genomic_ia <- function(x, sample = 0, blocksize = 100, quiet = FALSE){
   obs  <- bitwise.ia(x, threads = 1L)
   mat  <- as.matrix(x)
   if (sample == 0) return(c(rbarD = obs))
   exp  <- vector(mode = "numeric", length = sample)
   if (!quiet) p <- dplyr::progress_estimated(sample)
   for (i in seq(sample)){
-    exp[i] <- sample_bitwise_ia(mat, x)
+    exp[i] <- sample_bitwise_ia(mat, x, blocksize)
     if (!quiet) p$tick()$print()
   }
   if (!quiet) cat("\n")
