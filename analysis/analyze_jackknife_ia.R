a6797c5ea9b2527eb1b577624f8dd8ae42ed4a92
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2016-12-14 11:44:04 -0800

    Add file to analyze jackknifed data sets.

---
+++ analysis/analyze_jackknife_ia.R
@@ -0,0 +1,111 @@
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages(library("docopt"))
+"
+================================================================================
+Parse RDA files containing genclone objects, analyze clonecorrected IA vs.
+populations subsampled to the number of multilocus genotypes. A tibble is
+returned containing the fraction of jackknife data sets with a value of IA less
+than or equal to that of the clone-corrected data for both IA and rd, the result
+of the jackknife procedure, and the name of the population.
+
+Usage: analyze_jackknife_ia.R [-dvm -s SEED -p PERMUTATIONS -o PATH] [FILE...]
+
+Options:
+ -h,--help                                    show this message and exit
+ -v,--verbose                                 record progress
+ -d,--debug                                   record EVERYTHING
+ -m,--mutant                                  flag for indicating single mutant locus. If this is flagged, two analyses are run.
+ -s SEED,--seed=SEED                          random seed [default: 20160909]
+ -p PERMUTATIONS,--permutations=PERMUTATIONS  number of permutations for the index of association [default: 99]
+ -o PATH,--output=PATH                        default path to place Rdata files [default: ~/jackknife_rda_files]
+
+================================================================================
+" -> doc
+
+
+# Processing command line arguments ---------------------------------------
+
+opt <- docopt(doc, help = TRUE)
+
+if (length(opt$FILE) == 0){
+  message("\nFILE required. See options below\n")
+  docopt(doc, help = TRUE, args = "-h")
+}
+
+opt$seed         <- as.integer(opt$seed)
+opt$permutations <- as.integer(opt$permutations)
+
+if (!dir.exists(opt$output)){
+  dir.create(opt$output)
+}
+
+
+# Initializing packages ---------------------------------------------------
+
+if (opt$verbose){
+  the_options <- capture.output(print(opt))
+  the_options <- the_options[grepl("^ \\$ [[:alnum:]].+?$", the_options)]
+  message("The options:\n\n")
+  message(paste0(the_options, "\n"))
+  message("\nLoading packages ...\n")
+}
+suppressPackageStartupMessages(library("zksimanalysis"))
+suppressPackageStartupMessages(library("purrr"))
+
+
+# Processing data ---------------------------------------------------------
+
+
+# wrapper to exclude first locus before analyzing diversity.
+tidy_no_first_locus <- function(x, ...){
+  tidy_jackia(x[loc = -1, mlg.reset = TRUE], ...)
+}
+
+# For each file we will
+# 1. Load the file into R (since it will be an Rda file)
+# 2. Select the dataset column
+# 3. apply the tidy_diversity function to each data set
+# 4. store each result as a row in the table with pop as the key.
+# 5. save the results to the file name, replacing data with "divtable".
+for (f in opt$FILE){
+  indat <- load(f)
+  if (opt$verbose) message(paste("Analyzing", nrow(get(indat)), "populations ... "))
+  set.seed(opt$seed)
+  res <- indat %>%
+    get() %>%
+    select(dataset) %>%
+    apply(1, listfun, tidy_jackia,
+          reps = opt$permutations,
+          verbose = opt$debug,
+          quiet = !opt$verbose) %>%
+    bind_rows()
+  if (opt$mutant){
+    if (opt$verbose) message(paste("Analyzing populations without first locus ... "))
+    set.seed(opt$seed)
+    mres <- indat %>%
+      get() %>%
+      select(dataset) %>%
+      apply(1, listfun, tidy_no_first_locus,
+          reps = opt$permutations,
+          verbose = opt$debug,
+          quiet = !opt$verbose) %>%
+      bind_rows()
+  }
+  outf <- gsub(".DATA", ".jackia", basename(f))
+  assign(x = outf, res)
+  outf_location <- file.path(opt$output, outf)
+  if (opt$verbose) message(paste("saving results to", outf_location))
+  save(list = outf, file = outf_location)
+  if (opt$mutant){
+    moutf <- gsub(".DATA", ".jackia.mutant", basename(f))
+    assign(x = moutf, mres)
+    moutf_location <- file.path(opt$output, moutf)
+    if (opt$verbose) message(paste("saving results to", moutf_location))
+    save(list = moutf, file = moutf_location)
+  }
+}
+if (opt$verbose){
+  options(width = 100)
+  print(devtools::session_info())
+}
+
